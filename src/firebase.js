60268325ba51f82473e06bbc49a7954e9154c459
Devanshu121 <devanshujaiswal121@gmail.com>
2022-12-05 21:30:03 +0530

    Add your commit

---
+++ src/firebase.js
@@ -0,0 +1,43 @@
+/*import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDJdVfY0926CbLTO9VNK9cMDKiJ0TYa65o",
+  authDomain: "clone-c0687.firebaseapp.com",
+  databaseURL: "https://clone-c0687-default-rtdb.firebaseio.com",
+  projectId: "clone-c0687",
+  storageBucket: "clone-c0687.appspot.com",
+  messagingSenderId: "870644976594",
+  appId: "1:870644976594:web:005da729be9a6a52e6960b",
+  measurementId: "G-160ZB7MFE4",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
+*/
+
+import firebase from "firebase";
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDDDLysWG1tENMboq23_qbGo0IElJ_6BHs",
+  authDomain: "e-commerce-121.firebaseapp.com",
+  databaseURL: "https://e-commerce-121-default-rtdb.firebaseio.com",
+  projectId: "e-commerce-121",
+  storageBucket: "e-commerce-121.appspot.com",
+  messagingSenderId: "570201841918",
+  appId: "1:570201841918:web:1884efcb10a449bbc105af",
+  measurementId: "G-CJH2J8168P"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { db, auth };
